5fec23d2700e7dc0fb148f252f798919367c53bb
Thibault <thibault.moret@heig-vd.ch>
2026-02-18 15:41:29 +0100

    Création d'une base de donnée avec nos cours et modules, Modification de la page de nos projets web

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,5 @@
 -- Active: 1764341854783@@127.0.0.1@5437
 
-
 --CRÉER DES TABLES (💡 Ajouter IF NOT EXTISTS éviter les doublons)
 CREATE TABLE IF NOT EXISTS eleves (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -8,4 +7,18 @@ CREATE TABLE IF NOT EXISTS eleves (
     prenom TEXT NOT NULL,
     classe TEXT NOT NULL,
     date_naissance DATE
+);
+
+CREATE TABLE IF NOT EXISTS branches (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL, -- nom abrégé comme DocWeb
+    sujet TEXT NOT NULL, -- description du cours comme Développement Web (HTML/CSS)
+    id_module INT REFERENCES modules(id),
+    duree_semestre INT NOT NULL -- durée en demi semestre (1 à 4)
+);
+
+CREATE TABLE IF NOT EXISTS modules (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL,
+    secteur TEXT
 );
--- sql/requetes.sql
+++ sql/requetes.sql
@@ -1,4 +1,250 @@
-INSERT INTO eleves (nom, prenom, classe, date_naissance)
-VALUES ('Moret', 'Thibault', 'M54-2', '2005-07-05');
 
+--INSERT ZONE--
+INSERT INTO eleves (
+        nom,
+        prenom,
+        classe,
+        date_naissance
+    )
+VALUES (
+        'Moret',
+        'Thibault',
+        'M54-2',
+        '2005-07-05'
+    );
+
+INSERT INTO branches (
+        sujet,
+        nom,
+        id_module,
+        duree_semestre
+    )
+VALUES (
+        'Mathematiques',
+        'MathlM',
+        1,
+        4
+    ),
+    (
+        'Modelisation des donnees',
+        'ModelDon',
+        1,
+        2
+    ),
+    (
+        'Programmation',
+        'ProglM',
+        1,
+        4
+    ),
+    (
+        'Marketing digital',
+        'MarDig',
+        2,
+        4
+    ),
+    (
+        'Marketing et developpement de produit',
+        'MarDevProd',
+        2,
+        1
+    ),
+    (
+        'Recherche et analyse clients et publics cibles',
+        'RechAnPub',
+        2,
+        1
+    ),
+    (
+        'Auditer une communication numerique',
+        'AuditCom',
+        3,
+        1
+    ),
+    (
+        'Communication Serious Game',
+        'ComGame',
+        3,
+        1
+    ),
+    (
+        'Bases des medias numeriques',
+        'BasMedNum',
+        1,
+        1
+    ),
+    (
+        'Bases des neurosciences',
+        'BasNeuro',
+        1,
+        1
+    ),
+    (
+        'Durabilite',
+        'Dura',
+        4,
+        1
+    ),
+    (
+        'Sociologie des medias',
+        'Socio',
+        4,
+        1
+    ),
+    (
+        'Document web',
+        'DocuWeb',
+        5,
+        2
+    ),
+    (
+        'Outils de developpement',
+        'OutDev',
+        5,
+        1
+    ),
+    (
+        'Reseaux et environnement internet',
+        'ResInt',
+        5,
+        1
+    ),
+    (
+        'English for Engineers',
+        'Ang',
+        6,
+        4
+    ),
+    (
+        'Creation images',
+        'Crealm',
+        7,
+        2
+    ),
+    (
+        'Production de contenu media',
+        'ProdCont',
+        7,
+        1
+    ),
+    (
+        'Redaction et strategie de contenu',
+        'RedCont',
+        7,
+        1
+    ),
+    (
+        'Droit',
+        'Droit',
+        8,
+        1
+    ),
+    (
+        'Metiers des medias',
+        'MetMed',
+        8,
+        1
+    ),
+    (
+        'Infrastructure de donnees',
+        'InfraDon',
+        9,
+        2
+    ),
+    (
+        'Programmation serveur',
+        'ProgServ',
+        9,
+        1
+    ),
+    (
+        'Analyse de marche',
+        'AnalysMar',
+        2,
+        1
+    ),
+    (
+        'Developper une application web simple',
+        'DevAppliS',
+        3,
+        1
+    ),
+    (
+        'Mettre en place un ecosysteme digital',
+        'MEPEcosys',
+        3,
+        1
+    ),
+    (
+        'Conception orientee objet',
+        'ConcepOb',
+        1,
+        1
+    ),
+    (
+        'Outils methodologiques',
+        'OutMetho',
+        1,
+        1
+    ),
+    (
+        'Identite visuelle et systeme graphique',
+        'IdentVis',
+        10,
+        1
+    ),
+    (
+        'Interface utilisateur',
+        'IntUtil',
+        10,
+        1
+    ),
+    (
+        'Representation graphique des donnees',
+        'RepGraph',
+        10,
+        1
+    );
+
+INSERT INTO modules (nom, secteur)
+VALUES (
+        'Sciences ingenierie des medias',
+        'Technique'
+    ),
+    ('Marketing', 'Strategique'),
+    ('SAE', 'Pratique'),
+    (
+        'Sociologie et durabilite',
+        'Complementaire'
+    ),
+    (
+        'Technologies web',
+        'Technique'
+    ),
+    ('Anglais', 'Complementaire'),
+    ('Contenus medias', 'Creatif'),
+    (
+        'Droit et metiers des medias',
+        'Complementaire'
+    ),
+    (
+        'Developpement web',
+        'Technique'
+    ),
+    (
+        'Utilisateurs et interfaces',
+        'Strategique'
+    );
+
+
+
+--SELECT ZONE--
 SELECT * FROM eleves;
+
+SELECT * FROM branches;
+
+SELECT * FROM modules;
+
+SELECT b.nom, m.secteur
+FROM branches b
+    LEFT JOIN modules m ON m.id = b.id_module;
