56af95a019e3192050f819c1bff4ec03babcf983
Thibault <thibault.moret@heig-vd.ch>
2026-02-24 17:24:47 +0100

    feat: ajouté un compteur de projets web

---
+++ public/other/script.js
@@ -0,0 +1,13 @@
+// Functions
+
+function showProjectCount() {
+    let spanElement = document.getElementById("project-count"); // Va cherche l'élément avec l'id #project-count
+
+    var projectCount = document.getElementsByClassName("project").length; // Compte le nombre de projets sur la page
+
+    spanElement.textContent = projectCount; // Affiche ce nombre
+}
+
+// Actions
+
+showProjectCount();
